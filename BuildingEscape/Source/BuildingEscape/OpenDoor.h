8b3ac5f341272e40528904ab9349c492d9fa1af4
DanPaddock <danpaddk@gmail.com>
2018-07-20 19:52:37 -0400

    OpenDoor blueprint created

--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -7,6 +7,7 @@
 #include "Engine/TriggerVolume.h"
 #include "OpenDoor.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnOpenRequest);
 
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
@@ -26,16 +27,17 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-
+	UPROPERTY(BlueprintAssignable)
+	FOnOpenRequest OnOpenRequest;
 private:
 	UPROPERTY(EditAnywhere)
-		float OpenAngle = -90.0f;
+	float OpenAngle = -90.0f;
 
 	UPROPERTY(EditAnywhere)
-		ATriggerVolume* PressurePlate = nullptr ;
+	ATriggerVolume* PressurePlate = nullptr ;
 
 	UPROPERTY(EditAnywhere)
-		float DoorCloseDelay = 1.f;
+	float DoorCloseDelay = 1.f;
 
 	float LastDoorOpenTime;
 
